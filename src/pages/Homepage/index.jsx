236321974aa5aa80b1f8af0b11edb8ce723a9cb7
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2022-10-16 12:48:26 +0100

    Divide homepage in different and smaller components, styles for the menu-item component

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../components/Directory";
+
+import "./styles.scss";
+
+function Homepage() {
+	return (
+		<div className="homepage">
+			<Directory />
+		</div>
+	);
+}
+
+export default Homepage;
